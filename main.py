95c502eeddde1293d66cf9c2a8e517a7f72ba49e
Shrey Shailesh Shah <shreyss99@gmail.com>
2023-02-02 21:21:15 -0600

    Update main.py
    Updated main.py with main function to read command line argument

--- main.py
+++ main.py
@@ -3,10 +3,17 @@ import pandas as pd
 
 
 def convert_file_to_dataframe(file_name):
-    
+
     input_dataframe = pd.DataFrame()
     try:
         input_dataframe = pd.read_csv(file_name)
     except FileNotFoundError:
         print("The file {0} is not located in the current working directory".format(file_name))
     return input_dataframe
+
+
+if __name__ == '__main__':
+
+    points_total = int(sys.argv[1])
+
+
