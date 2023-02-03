412d96d3c8a6be0ee0a2842d1aaeb5afa7e98b58
Shrey Shailesh Shah <shreyss99@gmail.com>
2023-02-02 21:29:32 -0600

    Update main.py
    Updated the code and added docstring

--- main.py
+++ main.py
@@ -3,6 +3,18 @@ import pandas as pd
 
 
 def convert_file_to_dataframe(file_name):
+
+    """
+        Returns the dataframe created from the file_name specified
+
+                Parameters:
+                        file_name (str): A string storing the file_name
+
+                Returns:
+                        input_dataframe (DataFrame): The dataframe created from the file_name
+
+        """
+
     input_dataframe = pd.DataFrame()
     try:
         input_dataframe = pd.read_csv(file_name)
@@ -12,19 +24,19 @@ def convert_file_to_dataframe(file_name):
 
 
 def calculate_points(input_dataframe, input_points):
-    sorted_transactions = transactions.sort_values(by='timestamp', ignore_index=True)
+    sorted_dataframe = input_dataframe.sort_values(by='timestamp', ignore_index=True)
 
     i = 0
     while input_points > 0:
-        if input_dataframe.at[i, 'points'] > input_points:
-            input_dataframe.at[i, 'points'] -= input_points
+        if sorted_dataframe.at[i, 'points'] > input_points:
+            sorted_dataframe.at[i, 'points'] -= input_points
             break
         else:
-            input_points -= input_dataframe['points'][i]
-            input_dataframe.at[i, 'points'] -= input_dataframe.at[i, 'points']
+            input_points -= sorted_dataframe['points'][i]
+            sorted_dataframe.at[i, 'points'] -= sorted_dataframe.at[i, 'points']
         i += 1
 
-    final = input_dataframe.groupby('payer').sum('points')
+    final = sorted_dataframe.groupby('payer').sum('points')
     result = final.reset_index()
 
     return result
